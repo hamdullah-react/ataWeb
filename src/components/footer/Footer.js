81072a0165c1e0c55714ee8cb2b4cf4d7a92e1f6
reactteam <hamdullahs046@gmail.com>
2024-03-21 12:52:45 +0500

    update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,111 +3,120 @@ import React from 'react'
 
 const Footer = () => {
   return (
-   
-    <>
-  {/* Footer Start */}
-  <div className="container-fluid bg-secondary text-dark mt-5 pt-5">
-    <div className="row px-xl-5 pt-5">
-      <div className="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
-        <Link href={'./'} className="text-decoration-none">
-          <h1 className="mb-4 display-5 font-weight-semi-bold">
-            <span className="text-primary font-weight-bold border border-white px-3 mr-1">
-              Ghulam Haider
-            </span>
-             General Manager
-          </h1>
-        </Link>
 
-      
-        
-        <p className="mb-0">
-          <i className="fa-solid fa-mobile text-primary mr-3"></i>
-          +971 56 2215 770
-        </p>
-        <p className="mb-0">
-          
-          <i className="fa fa-phone-alt text-primary mr-3" />
-          +971 4 33 27 424
-        </p>
-        <p className="mb-2">
-          <i className="fa fa-envelope text-primary mr-3" />
-          haider@mgssuae.com
-        </p>
-        <p className="mb-2">
-          <i className="fa fa-map-marker-alt text-primary mr-3" />
-          Mustafa Mohd.Abdul Noor Bldg.Behind Golden Square Hotel Nakhee Road,Deira,Dubai - U.A.E.
-        </p>
-        
-      </div>
-      <div className="col-lg-8 col-md-12">
-        <div className="row">
-          <div className="col-md-4 mb-5">
-            <h5 className="font-weight-bold text-dark mb-4">Quick Links</h5>
-            <div className="d-flex flex-column justify-content-start">
-              <Link className="text-dark mb-2" href="/">
-                <i className="fa fa-angle-right mr-2" />
-                Home
-              </Link>
-              <Link className="text-dark mb-2" href="/aboutus">
-                <i className="fa fa-angle-right mr-2" />
-                About Us
-              </Link>
-              <Link className="text-dark mb-2" href="/contactus">
-                <i className="fa fa-angle-right mr-2" />
-               Contact Us
-              </Link>
-            </div>
+    <>
+      {/* Footer Start */}
+      <div className="container-fluid bg-secondary text-dark mt-5 pt-5">
+        <div className="row px-xl-5 pt-5">
+          <div className="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
+            <Link href={'./'} className="text-decoration-none">
+              <h1 className="mb-4 display-5 font-weight-semi-bold">
+                <span className="text-primary font-weight-bold border border-white px-3 mr-1">
+                  Ghulam Haider
+                </span>
+                General Manager
+              </h1>
+            </Link>
+
+
+
+            <p className="mb-0">
+              <i className="fa-solid fa-mobile text-primary mr-3"></i>
+              +971 56 2215 770
+            </p>
+            <p className="mb-0">
+
+              <i className="fa fa-phone-alt text-primary mr-3" />
+              +971 4 33 27 424
+            </p>
+            <p className="mb-2">
+              <i className="fa fa-envelope text-primary mr-3" />
+              haider@mgssuae.com
+            </p>
+            <p className="mb-2">
+              <i className="fa fa-map-marker-alt text-primary mr-3" />
+              Mustafa Mohd.Abdul Noor Bldg.Behind Golden Square Hotel Nakhee Road,Deira,Dubai - U.A.E.
+            </p>
+
           </div>
-          <div className="col-md-4 mb-5">
-            <h5 className="font-weight-bold text-dark mb-4">Quick Links</h5>
-            <div className="d-flex flex-column justify-content-start">
-            <Link className="text-dark mb-2" href="/">
-                <i className="fa fa-angle-right mr-2" />
-                Home
-              </Link>
-              <Link className="text-dark mb-2" href="/aboutus">
-                <i className="fa fa-angle-right mr-2" />
-                About Us
-              </Link>
-              <Link className="text-dark mb-2" href="/contactus">
-                <i className="fa fa-angle-right mr-2" />
-               Contact Us
-              </Link>
+          <div className="col-lg-8 col-md-12">
+            <div className="row">
+              <div className="col-md-4 mb-5">
+                <h5 className="font-weight-bold text-dark mb-4">Quick Links</h5>
+                <div className="d-flex flex-column justify-content-start">
+                  <Link className="text-dark mb-2" href="/">
+                    <i className="fa fa-angle-right mr-2" />
+                    Home
+                  </Link>
+                  <Link className="text-dark mb-2" href="/products">
+                    <i className="fa fa-angle-right mr-2" />
+                    All Products
+                  </Link>
+                  
+                  <Link className="text-dark mb-2" href="/aboutus">
+                    <i className="fa fa-angle-right mr-2" />
+                    About Us
+                  </Link>
+                  <Link className="text-dark mb-2" href="/contactus">
+                    <i className="fa fa-angle-right mr-2" />
+                    Contact Us
+                  </Link>
+                </div>
+              </div>
+              <div className="col-md-4 mb-5">
+                <h5 className="font-weight-bold text-dark mb-4">Quick Links</h5>
+                <div className="d-flex flex-column justify-content-start">
+                  <Link className="text-dark mb-2" href="/">
+                    <i className="fa fa-angle-right mr-2" />
+                    Home
+                  </Link>
+                  <Link className="text-dark mb-2" href="/products">
+                    <i className="fa fa-angle-right mr-2" />
+                    All Products
+                  </Link>
+                  <Link className="text-dark mb-2" href="/aboutus">
+                    <i className="fa fa-angle-right mr-2" />
+                    About Us
+                  </Link>
+                  <Link className="text-dark mb-2" href="/contactus">
+                    <i className="fa fa-angle-right mr-2" />
+                    Contact Us
+                  </Link>
+                </div>
+              </div>
+              <div className="col-md-4 mb-5">
+                <h5 className="font-weight-bold text-dark mb-4">Newsletter</h5>
+                <form action="">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control border-0 py-4"
+                      placeholder="Your Name"
+                      required="required"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control border-0 py-4"
+                      placeholder="Your Email"
+                      required="required"
+                    />
+                  </div>
+                  <div>
+                    <button
+                      className="btn btn-primary btn-block border-0 py-3"
+                      type="submit"
+                    >
+                      Subscribe Now
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
-          </div>
-          <div className="col-md-4 mb-5">
-            <h5 className="font-weight-bold text-dark mb-4">Newsletter</h5>
-            <form action="">
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control border-0 py-4"
-                  placeholder="Your Name"
-                  required="required"
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="email"
-                  className="form-control border-0 py-4"
-                  placeholder="Your Email"
-                  required="required"
-                />
-              </div>
-              <div>
-                <button
-                  className="btn btn-primary btn-block border-0 py-3"
-                  type="submit"
-                >
-                  Subscribe Now
-                </button>
-              </div>
-            </form>
           </div>
         </div>
-      </div>
-    </div>
-    {/* <div className="row border-top border-light mx-xl-5 py-4">
+        {/* <div className="row border-top border-light mx-xl-5 py-4">
       <div className="col-md-6 px-xl-0">
         <p className="mb-md-0 text-center text-md-left text-dark">
           ©{" "}
@@ -132,9 +141,9 @@ const Footer = () => {
         <img className="img-fluid" src="img/payments.png" alt="" />
       </div>
     </div> */}
-  </div>
-  {/* Footer End */}
-</>
+      </div>
+      {/* Footer End */}
+    </>
 
   )
 }
--- src/components/slider/Headerslider.js
+++ src/components/slider/Headerslider.js
@@ -1,43 +1,77 @@
-'use client'
-import Image from 'next/image';
-import { useState } from 'react';
+"use client";
+import Image from "next/image";
+import { useState } from "react";
 
 const MyCarousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   const handlePrev = () => {
-    setActiveIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
   };
 
   const handleNext = () => {
-    setActiveIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    setActiveIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   const images = [
-    { src: '/img/1.jpeg', alt: 'First slide', height: 500 }, // Reduced height for responsiveness
-    { src: '/img/4.jpeg', alt: 'Second slide', height: 500 } // Reduced height for responsiveness
+    { src: "/img/1-removebg-preview.png", alt: "First slide", height: 500 }, // Reduced height for responsiveness
+    { src: "/img/4-removebg-preview.png", alt: "Second slide", height: 500 }, // Reduced height for responsiveness
+    { src: "/img/3-removebg-preview.png", alt: "Second slide", height: 500 }, // Reduced height for responsiveness
   ];
 
   return (
     <>
-    <div id="header-carousel" className="carousel slide" data-bs-ride="carousel">
-      <div className="carousel-inner">
-        {images.map((image, index) => (
-          <div key={index} className={`carousel-item${index === activeIndex ? ' active' : ''}`} style={{ height: image.height }}>
-            <Image src={image.src} alt={image.alt} layout="fill" style={{objectFit:'scale-down'}} />
+      <div
+        id="header-carousel"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={`carousel-item${
+                index === activeIndex ? " active" : ""
+              }`}
+              style={{ height: image.height }}
+            >
+              <Image
+                src={image.src}
+                alt={image.alt}
+                layout="fill"
+                style={{ objectFit: "scale-down" }}
+              />
+            </div>
+          ))}
+        </div>
+   
+        <span
+        
+          className="carousel-control-prev"
+          data-slide="prev"
+          onClick={handlePrev}
+        >
+          <div className="btn btn-dark" style={{ width: 45, height: 45 }}>
+            <span className="carousel-control-prev-icon mb-n2" />
           </div>
-        ))}
-      </div>
-      <button className="carousel-control-prev" type="button" onClick={handlePrev} style={{ color: 'black' }}>
-        <span className="carousel-control-prev-icon" aria-hidden="true" style={{ color: 'black' }} />
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button className="carousel-control-next" type="button" onClick={handleNext} style={{ color: 'black' }}>
-        <span className="carousel-control-next-icon" aria-hidden="true" style={{ color: 'black' }} />
-        <span className="visually-hidden">Next</span>
-      </button>
-    </div>
+        </span>
+        <span
+         onClick={handleNext}
+          className="carousel-control-next"
+          
+          data-slide="next"
+        >
+          <div className="btn btn-dark" style={{ width: 45, height: 45 }}>
+            <span className="carousel-control-next-icon mb-n2" />
+          </div>
+        </span>
     
+      </div>
+
     </>
   );
 };
