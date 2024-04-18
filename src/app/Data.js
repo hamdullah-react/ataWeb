dcdba7d1c396a87393f0b779c3c6d7de896c6d6f
reactteam <hamdullahs046@gmail.com>
2024-04-18 14:30:26 +0500

    updated

--- src/app/Data.js
+++ src/app/Data.js
@@ -8,6 +8,7 @@ export const categories = [
         { id: 1, name: "17CM POLL", img: "img/categorie/camerapolls/1.png" },
         { id: 2, name: "Camra Polls", img: "img/categorie/camerapolls/2.png" },
         { id: 3, name: "JUNCTION BOXES", img: "img/categorie/camerapolls/3.png" },
+        { id: 4, name: "L DASH BIG , L DASH SMALL ", img: "img/categorie/camerapolls/4.png" },
         
      
     ],
@@ -17,9 +18,9 @@ export const categories = [
       name: "Cables",
       data: [
         { id: 1, name: "DC CABLE MALE , DC CABLE FEMALE", img: "img/categorie/cable/1.png" },
-        { id: 2, name: "Cables", img: "img/categorie/2.png" },
-        { id: 3, name: "Cables", img: "img/categorie/4.png" },
-        { id: 3, name: "Cables", img: "img/categorie/114.png" },
+        { id: 2, name: "HDMI cable 1.5METER", img: "img/categorie/cable/2.png" },
+        { id: 3, name: "HDMI CABLE 1.5METER 4K", img: "img/categorie/cable/3.png" },
+   
        
       ],
     },
@@ -27,26 +28,13 @@ export const categories = [
       id: 3,
       name: "Devices",
       data: [
-        { id: 1, name: "BNS Crimping tool", img: "img/categorie/101.png" },
-        { id: 2, name: "RJ45 Steeper", img: "img/categorie/102.png" },
-        { id: 3, name: "RJ45 Crimping tool", img: "img/categorie/103.png" },
-        { id: 4, name: "", img: "img/categorie/104.png" },
-        { id: 5, name: "", img: "img/categorie/105.png" },
-        { id: 6, name: "", img: "img/categorie/106.png" },
-        { id: 7, name: "", img: "img/categorie/107.png" },
-        { id: 8, name: "", img: "img/categorie/108.png" },
-        { id: 9, name: "", img: "img/categorie/109.png" },
-        { id: 10, name: "", img: "img/categorie/110.png" },
-        { id: 11, name: "", img: "img/categorie/111.png" },
-        { id: 12, name: "", img: "img/categorie/113.png" },
-
-        { id: 13, name: "KVM 120 Meter", img: "img/categorie/devices/1.png" },
-        { id: 14, name: "KVM 200 Meter", img: "img/categorie/devices/2.png" },
-        { id: 15, name: "HDMI SPLITTER ", img: "img/categorie/devices/3.png" },
-        { id: 16, name: "mouse", img: "img/categorie/devices/4.png" },
-        { id: 17, name: "HDMI extender 30 METER ", img: "img/categorie/devices/5.png" },
-        { id: 18, name: "WIRE TRACKER  ", img: "img/categorie/devices/6.png" },
-        { id: 19, name: "RJ45 NETWORK CABLE TESTER   ", img: "img/categorie/devices/7.png" },
+        { id: 1, name: "KVM 120 Meter", img: "img/categorie/devices/1.png" },
+        { id: 2, name: "KVM 200 Meter", img: "img/categorie/devices/2.png" },
+        { id: 3, name: "HDMI SPLITTER ", img: "img/categorie/devices/3.png" },
+        { id: 4, name: "mouse", img: "img/categorie/devices/4.png" },
+        { id: 5, name: "HDMI extender 30 METER ", img: "img/categorie/devices/5.png" },
+        { id: 6, name: "WIRE TRACKER  ", img: "img/categorie/devices/6.png" },
+        { id: 7, name: "RJ45 NETWORK CABLE TESTER   ", img: "img/categorie/devices/7.png" },
 
 
         
