760ce94bc15991015eb018175a87810719885097
reactteam <hamdullahs046@gmail.com>
2024-04-18 12:07:54 +0500

    d

--- src/app/Data.js
+++ src/app/Data.js
@@ -6,6 +6,12 @@ export const categories = [
       data: [
         { id: 1, name: "Camra Polls", img: "img/categorie/6.png" },
         { id: 2, name: "", img: "img/categorie/112.png" },
+
+
+        { id: 3, name: "17CM POLL", img: "img/categorie/camerapolls/1.png" },
+        { id: 3, name: "Camra Polls", img: "img/categorie/camerapolls/2.png" },
+        { id: 3, name: "JUNCTION BOXES", img: "img/categorie/camerapolls/3.png" },
+        
      
     ],
     },
@@ -42,6 +48,8 @@ export const categories = [
         { id: 15, name: "HDMI SPLITTER ", img: "img/categorie/devices/3.png" },
         { id: 16, name: "mouse", img: "img/categorie/devices/4.png" },
         { id: 17, name: "HDMI extender 30 METER ", img: "img/categorie/devices/5.png" },
+        { id: 18, name: "WIRE TRACKER  ", img: "img/categorie/devices/6.png" },
+        { id: 19, name: "RJ45 NETWORK CABLE TESTER   ", img: "img/categorie/devices/7.png" },
 
 
         
@@ -52,8 +60,9 @@ export const categories = [
       id: 4,
       name: "Adaptor & Power Supply",
       data: [
-        { id: 1, name: "Power Supply", img: "img/categorie/5.png" },
-        { id: 1, name: "Power Supply", img: "img/categorie/3.png" },
+        { id: 1, name: "12V 5AH Adaptor ,  12V 10AH Adaptor", img: "img/categorie/adopter&powersupply/1.png" },
+        { id: 2, name: "12V20AH +18CHANNEL  POWER SUPPLY , 12V30AH +18CHANNEL POWER SUPPLY", img: "img/categorie/adopter&powersupply/2.png" },
+      
       ],
     },
     {
@@ -61,8 +70,12 @@ export const categories = [
       name: "Connecter",
       data: [
         { id: 1, name: "BNC Spring", img: "img/categorie/connecter/1.png" },
-        { id: 1, name: "BNC Crimping", img: "img/categorie/connecter/2.png" },
-        { id: 1, name: "CAT6 TO BNC BALOON", img: "img/categorie/connecter/3.png" },
+        { id: 2, name: "BNC Crimping", img: "img/categorie/connecter/2.png" },
+        { id: 3, name: "CAT6 TO BNC BALOON", img: "img/categorie/connecter/3.png" },
+        { id: 4, name: "BALLOON 2MP", img: "img/categorie/connecter/4.png" },
+        { id: 5, name: "BALLOON 8MP", img: "img/categorie/connecter/5.png" },
+        { id: 6, name: "BNC SPRING ", img: "img/categorie/connecter/6.png" },
+        { id: 7, name: "CRIMPING TOOLS ", img: "img/categorie/connecter/7.png" },
       
       ],
     },
